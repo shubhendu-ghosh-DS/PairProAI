8e1c56684b254ca03c026abe44411b1250076418
shubhendu ghosh <shubhendu-ghosh@users.noreply.huggingface.co>
2025-04-02 13:21:28 +0000

    Update app/generate_code.py

--- app/generate_code.py
+++ app/generate_code.py
@@ -4,7 +4,7 @@ from app.config import Config
 from app.utils import create_prompt, extract_code_and_explanation
 
 genai.configure(api_key=Config.API_KEY)
-model = genai.GenerativeModel(model_name="gemini-1.5-flash")
+model = genai.GenerativeModel(model_name="gemini-2.0-flash")
 
 def generate_code(code, option, target_language=None, error=None):
     try:

94228736671c4de220c72fae4919bff4f1813b97
shubhendu ghosh <shubhendu-ghosh@users.noreply.huggingface.co>
2025-04-02 13:12:58 +0000

    Create app/__init__.py

---
+++ app/__init__.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from app.routes import main
+from app.config import Config
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+    
+    app.register_blueprint(main)
+    
+    return app
